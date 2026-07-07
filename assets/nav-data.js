57470c965646ccbcfc84e76b5929163288048bb7
LisanProject <lisanproject25@gmail.com>
2026-07-06 20:13:20 -0700

    Update nav-data.js

--- assets/nav-data.js
+++ assets/nav-data.js
@@ -387,7 +387,8 @@ var NAV = {
     { title: "Khutbah a Week", path: "/Extras/Khutbah.html" },
     { title: "Essays", path: "/Extras/Essays.html" },
     { title: "Reading Recommendations", path: "/Extras/ReadingRecs.html" },
-    { title: "Ijazat", path: "/Classes/Ijazat/Ijazat.html" }
+    { title: "Ijazat", path: "/Classes/Ijazat/Ijazat.html" },
+   {  title: "Daily Motivation from Irshad al-'Ibad", path: "/Extras/DailyMotivation.html" }
   ]
 };
 
